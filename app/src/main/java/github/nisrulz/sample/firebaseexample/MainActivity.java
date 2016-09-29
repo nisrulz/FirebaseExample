763d5be79882c40f77eff502d2eeb25c2aed581a
Nishant Srivastava <nisrulz@gmail.com>
2016-09-29 16:28:36 +0530

    [*] init firebase analytics and added required permissions

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
+
+  // Declare FirebaseAnalytics
+  private FirebaseAnalytics firebaseAnalytics;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    // Init FirebaseAnalytics
+    firebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
