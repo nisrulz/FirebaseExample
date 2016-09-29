795b77fd4767d4a44b040b54c75ec7cecb738417
Nishant Srivastava <nisrulz@gmail.com>
2016-09-29 16:19:02 +0530

    [*] implemented crash reporting as well as custom event logs

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import com.google.firebase.crash.FirebaseCrash;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +20,28 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
+    // Log Custom Event
+    FirebaseCrash.log("Activity created");
+
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
+        Snackbar.make(view, "Cause a Runtime Exception ?", Snackbar.LENGTH_LONG)
+            .setAction("Yes", new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                try {
+                  // Cause a Runtime Exception
+                  throw new RuntimeException("My first Android Runtime Exception");
+                } catch (Exception e) {
+                  Toast.makeText(MainActivity.this, "Runtime Exception caused and caught",
+                      Toast.LENGTH_SHORT).show();
+                  // Report the crash
+                  FirebaseCrash.report(e);
+                }
+              }
+            })
             .show();
       }
     });
