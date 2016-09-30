ecf0a1af06ef23014b14bdc42a8e6d594b2241d5
Nishant Srivastava <nisrulz@gmail.com>
2016-09-30 20:53:03 +0530

    [*] setup signup or registration

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
@@ -1,15 +1,34 @@
 package github.nisrulz.sample.firebaseexample;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+  //defining view objects
+  private EditText editTextEmail;
+  private EditText editTextPassword;
+  private Button buttonSignup;
+  private ProgressDialog progressDialog;
+
+  //defining firebaseauth object
+  private FirebaseAuth firebaseAuth;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    //initializing firebase auth object
+    firebaseAuth = FirebaseAuth.getInstance();
+
+    //initializing views
+    editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+    editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+    buttonSignup = (Button) findViewById(R.id.buttonSignup);
+
+    progressDialog = new ProgressDialog(this);
+
+    //attaching listener to button
+    buttonSignup.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        registerUser();
+      }
+    });
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +65,49 @@ public class MainActivity extends AppCompatActivity {
             .show();
       }
     });
+  }
+
+  private void registerUser() {
+
+    //getting email and password from edit texts
+    String email = editTextEmail.getText().toString().trim();
+    String password = editTextPassword.getText().toString().trim();
+
+    //checking if email and passwords are empty
+    if (TextUtils.isEmpty(email)) {
+      Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    if (TextUtils.isEmpty(password)) {
+      Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    //if the email and password are not empty
+    //displaying a progress dialog
+
+    progressDialog.setMessage("Registering Please Wait...");
+    progressDialog.show();
+
+    //creating a new user
+    firebaseAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+            //checking if success
+            if (task.isSuccessful()) {
+              //display some message here
+              Toast.makeText(MainActivity.this, "Successfully registered", Toast.LENGTH_LONG)
+                  .show();
+            }
+            else {
+              //display some message here
+              Toast.makeText(MainActivity.this, "Registration Error", Toast.LENGTH_LONG).show();
+            }
+            progressDialog.dismiss();
+          }
+        });
   }
 
   @Override
