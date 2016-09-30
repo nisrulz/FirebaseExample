40a2d3632f93a2f94cfe5f7a38eda7703a3b3ca4
Nishant Srivastava <nisrulz@gmail.com>
2016-09-30 16:37:00 +0530

    [*] setup textview

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  // declare textview
+  TextView txt_hello;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    // Init textview
+    txt_hello = (TextView) findViewById(R.id.txt_hello);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
