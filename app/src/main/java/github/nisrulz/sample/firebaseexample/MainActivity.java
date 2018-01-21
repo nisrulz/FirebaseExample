59ca90d3a707dae79a116b4fb884ff04a66cc4eb
Nishant Srivastava <nisrulz@gmail.com>
2018-01-21 19:01:30 +0530

    updated: code cleanup and updated dependencies

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
@@ -3,18 +3,15 @@ package github.nisrulz.sample.firebaseexample;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
     // Init FirebaseRemoteConfig
@@ -76,20 +73,10 @@ public class MainActivity extends AppCompatActivity {
             }
           }
 
-        }
-        else {
+        } else {
           //task failed
+          Toast.makeText(MainActivity.this, "RemoteConfig Task Failed!", Toast.LENGTH_SHORT).show();
         }
-      }
-    });
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
       }
     });
   }
@@ -100,28 +87,5 @@ public class MainActivity extends AppCompatActivity {
     String isSadString = remoteConfig.getString("is_sad");
 
     Toast.makeText(this, isHappyString + "\n" + isSadString, Toast.LENGTH_LONG).show();
-  }
-
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 }
