b17f24fa2c0ff1dfd5ca5eb2ec35de13a17a8880
Nishant Srivastava <nisrulz@gmail.com>
2018-01-21 20:23:56 +0530

    updated: code cleanup and updated dependencies

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MyFirebaseMessagingService.java
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MyFirebaseMessagingService.java
@@ -1,54 +1,49 @@
 package github.nisrulz.sample.firebaseexample;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
   private static final String TAG = "FCM";
 
   @Override
   public void onMessageReceived(RemoteMessage remoteMessage) {
-    // ...
-
-    // TODO(developer): Handle FCM messages here.
     // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-    Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-    // Check if message contains a data payload.
-    if (remoteMessage.getData().size() > 0) {
-      Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+    if (remoteMessage != null) {
+      String fromStr = remoteMessage.getFrom();
+      Map<String, String> payloadMap = remoteMessage.getData();
+      String bodyStr = "NA";
+
+      // From String
+      if (fromStr != null) {
+        Log.d(TAG, "From: " + fromStr);
+      }
+
+      // Check if message contains a data payload.
+      if (payloadMap.size() > 0) {
+        Log.d(TAG, "Payload: ");
+        for (String key : payloadMap.keySet()) {
+          Log.d(TAG, "Key: " + key + ", Value: " + payloadMap.get(key));
+        }
+      }
+
+      // Check if message contains a notification payload.
+      if (remoteMessage.getNotification() != null) {
+        bodyStr = remoteMessage.getNotification().getBody();
+        Log.d(TAG, "Body: " + bodyStr);
+      }
+
+      // Also if you intend on generating your own notifications as a result of a received FCM
+      // message, here is where that should be initiated.
+      NotificationUtils notificationUtils = new NotificationUtils(this);
+      notificationUtils.sendAndroidChannelNotification("From: " + fromStr, bodyStr, 101);
     }
 
-    // Check if message contains a notification payload.
-    if (remoteMessage.getNotification() != null) {
-      String msg = "Message Notification Body: " + remoteMessage.getNotification().getBody();
-      Log.d(TAG, msg);
-    }
 
-    // Also if you intend on generating your own notifications as a result of a received FCM
-    // message, here is where that should be initiated.
-
-    showNotification(remoteMessage.getNotification().getBody(), remoteMessage.getFrom());
-  }
-
-  private void showNotification(String body, String title) {
-    final Intent emptyIntent = new Intent();
-    PendingIntent pendingIntent =
-        PendingIntent.getActivity(this, 1, emptyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-    NotificationCompat.Builder mBuilder =
-        new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setContentIntent(pendingIntent); //Required on Gingerbread and below
-
-    NotificationManager notificationManager =
-        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.notify(2, mBuilder.build());
   }
 }
