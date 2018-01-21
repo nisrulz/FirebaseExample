66b39696fd546a64e625b710b9c7523573dbe047
Nishant Srivastava <nisrulz@gmail.com>
2018-01-21 20:45:45 +0530

    updated: code cleanup and updated dependencies

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.java
@@ -5,9 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,55 +18,40 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
     // Init FirebaseAnalytics
     firebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    FloatingActionButton fab = findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
+
+        // log to analytics
+        logEventInFirebaseAnalytics();
+
+        // show snackbar
         Snackbar.make(view, "Sending Analytics data", Snackbar.LENGTH_LONG)
             .setAction("OK", null)
             .show();
-
-        // Log event
-        firebaseAnalytics.logEvent("checkout_complete", null);
-
-        // Log parametrized event
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Pizza");
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
-
-        // Log User Property
-        firebaseAnalytics.setUserProperty("favourite_food", "Pizza");
       }
     });
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
+  private void logEventInFirebaseAnalytics() {
+    // Log event
+    firebaseAnalytics.logEvent("checkout_complete", null);
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
+    // Log parametrized event
+    Bundle bundle = new Bundle();
+    bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
+    bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Pizza");
+    bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+    firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
 
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
+    // Log User Property
+    firebaseAnalytics.setUserProperty("favourite_food", "Pizza");
   }
 }
