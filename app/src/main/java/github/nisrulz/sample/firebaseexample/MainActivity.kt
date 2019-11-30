be94c12ffef9662770fc65781025fb7751a3c947
Nishant Srivastava <nisrulz@gmail.com>
2019-11-30 23:51:49 +0100

    updated code for remote config

--- app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.kt
+++ app/src/main/java/github/nisrulz/sample/firebaseexample/MainActivity.kt
@@ -3,6 +3,7 @@ package github.nisrulz.sample.firebaseexample
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -23,8 +24,15 @@ class MainActivity : AppCompatActivity() {
     toolbar = findViewById(R.id.toolbar)
     setSupportActionBar(toolbar)
 
-    val remoteConfigSettings = FirebaseRemoteConfigSettings.Builder().build()
-    remoteConfig.setConfigSettingsAsync(remoteConfigSettings)
+    // Setup the remote config settings
+    FirebaseRemoteConfigSettings.Builder().also {
+      if (BuildConfig.DEBUG) {
+        it.setMinimumFetchIntervalInSeconds(3600)
+      } else {
+        it.setMinimumFetchIntervalInSeconds(2)
+      }
+      remoteConfig.setConfigSettingsAsync(it.build())
+    }
 
     // Set Defaults
     val defaults = HashMap<String, Any>()
@@ -34,42 +42,41 @@ class MainActivity : AppCompatActivity() {
     defaults["color_primary_dark"] = "#303F9F"
     remoteConfig.setDefaultsAsync(defaults)
 
-    // Configure remote config to fetch updated configurations
-    // cache expiration in seconds
-    var cacheExpiration: Long = 3600
-
-    //expire the cache immediately for development mode.
-    if (BuildConfig.DEBUG) {
-      cacheExpiration = 0
-    }
-
     // fetch
-    remoteConfig.fetch(cacheExpiration).addOnCompleteListener(this) { task ->
+    remoteConfig.fetchAndActivate().addOnCompleteListener(this) { task ->
       if (task.isSuccessful) {
-        // task successful. Activate the fetched data
-        remoteConfig.fetchAndActivate()
+        val updated = task.result
 
-        //update views?
+        Log.d("FirebaseRemoteConfig", "Config params updated: $updated")
+        showToast("Fetch and activate succeeded")
+
+        //update views
         updateViews()
-
-        // update the toolbar
-        toolbar?.setBackgroundColor(Color.parseColor(remoteConfig.getString("color_primary")))
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-          window.statusBarColor = Color.parseColor(remoteConfig.getString("color_primary_dark"))
-        }
-
       } else {
         //task failed
-        Toast.makeText(this@MainActivity, "RemoteConfig Task Failed!", Toast.LENGTH_SHORT).show()
+        showToast("RemoteConfig Task Failed! ${task.exception}")
       }
     }
   }
 
   private fun updateViews() {
     // check whether promo is on
-    val isHappyString = remoteConfig.getString("is_happy")
-    val isSadString = remoteConfig.getString("is_sad")
+    remoteConfig.apply {
+      val isHappyString = getString("is_happy")
+      val isSadString = getString("is_sad")
 
-    Toast.makeText(this, isHappyString + "\n" + isSadString, Toast.LENGTH_LONG).show()
+      showToast(isHappyString + "\n" + isSadString)
+
+      // update the toolbar
+      toolbar?.setBackgroundColor(Color.parseColor(getString("color_primary")))
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        window.statusBarColor = Color.parseColor(getString("color_primary_dark"))
+      }
+    }
+
+  }
+
+  private fun showToast(str: String) {
+    Toast.makeText(applicationContext, str, Toast.LENGTH_LONG).show()
   }
 }
